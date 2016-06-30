0ffd2244f1be404321bea42416acc0d14b451b50
inthegully <fritzty@gmail.com>
2016-06-29 23:09:20 -0600

    refactored to satisfaction

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
 $(document).ready(function() {
+
     $('input').keypress(function(e) {
         if (e.which == 13) {
             var artistName = $('input').val();
-            var artistId;
-            getArtist();
+            createPlaylist(artistName);
 
-            function getArtist() {
-                $.ajax({
-                    method: "GET",
-                    url: "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist",
-                    success: function(response) {
-                        artistId = response.artists.items[0].id
-                        getRelatedArtists();
-                    }
-                });
-            }
-            function getRelatedArtists(){
-              $.ajax({
-                method: "GET",
-                url: "https://api.spotify.com/v1/artists/" + artistId + "/related-artists",
-                success: function(response) {
-                    for (var i = 0; i < 10; i++) {
-                      var relatedId = response.artists[i].id
-                      $.ajax({
-                        method: "GET",
-                        url: "https://api.spotify.com/v1/artists/" + relatedId + "/top-tracks?country=US",
-                        success: function(response) {
-                          var topTrack = response.tracks[0]
-                          $('.allSongs').append("<div class='playlist'><a href=" + topTrack.uri + " class='song'>" + topTrack.name + " - " + topTrack.artists[0].name + "</a></div>")
-                        }
-                      });
-                    }
-                }
-              });
-            }
             $('input').val('');
             $('.allSongs').empty();
         }
     });
+
+    function createPlaylist(artistName) {
+        getArtist(artistName);
+
+        function getArtist(artistName) {
+            $.ajax({
+                method: "GET",
+                url: "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist",
+                success: function(response) {
+                    var artistId = response.artists.items[0].id
+                    getRelatedArtists(artistId);
+                }
+            });
+        }
+
+        function getRelatedArtists(artistId) {
+            $.ajax({
+                method: "GET",
+                url: "https://api.spotify.com/v1/artists/" + artistId + "/related-artists",
+                success: function(response) {
+                    getTracks(response);
+                }
+            });
+        }
+
+        function getTracks(related) {
+            for (var i = 0; i < 10; i++) {
+                var relatedId = related.artists[i].id
+                $.ajax({
+                    method: "GET",
+                    url: "https://api.spotify.com/v1/artists/" + relatedId + "/top-tracks?country=US",
+                    success: function(response) {
+                        postTrack(response);
+                    }
+                });
+            }
+        }
+
+        function postTrack(relatedArtist) {
+            var topTrack = relatedArtist.tracks[0]
+            $('.allSongs').append("<div class='playlist'><a href=" + topTrack.uri + " class='song'>" + topTrack.name + " - " + topTrack.artists[0].name + "</a></div>")
+        }
+    }
 });
