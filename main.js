9a0aaff0c4bea2774e30c3bce1eb9b37335380fc
Tiffany Fritz <fritzty@gmail.com>
2017-07-13 12:46:50 -0600

    did authorization

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+    function login(){
+      var clientId = '1a3e921e1c1c47a0be21759f53566991';
+      var responseType = '&response_type=token';
+      var redirectURI = 'http://dimpledelltestsite.site44.com';
+
+      var URL = 'https://accounts.spotify.com/authorize?client_id=' +
+                clientId +
+                responseType +
+                '&redirect_uri=' +
+                redirectURI;
+
+      return window.location.replace(URL);
+    }
+
+    if(window.location.href.indexOf("access_token") == -1) {
+      login();
+    }else{
+      var accessToken = getHashValue("access_token");
+
+      function getHashValue(key){
+        var matches = location.hash.match(new RegExp(key+"=([^&]*)"));
+        return matches ? matches[1] : null;
+      }
+    }
+
+
     $('input').keypress(function(e) {
         if (e.which == 13) {
             var artistName = $('input').val();
@@ -17,6 +43,9 @@ $(document).ready(function() {
             $.ajax({
                 method: "GET",
                 url: "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist",
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
                 success: function(response) {
                     var artistId = response.artists.items[0].id
                     getRelatedArtists(artistId);
@@ -28,6 +57,9 @@ $(document).ready(function() {
             $.ajax({
                 method: "GET",
                 url: "https://api.spotify.com/v1/artists/" + artistId + "/related-artists",
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
                 success: function(response) {
                     getTracks(response);
                 }
@@ -40,6 +72,9 @@ $(document).ready(function() {
                 $.ajax({
                     method: "GET",
                     url: "https://api.spotify.com/v1/artists/" + relatedId + "/top-tracks?country=US",
+                    headers: {
+                        'Authorization': 'Bearer ' + accessToken
+                    },
                     success: function(response) {
                         postTrack(response);
                     }
